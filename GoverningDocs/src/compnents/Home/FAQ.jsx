5deb35e6ec79e1d8fa0e879c946ff318580f8be6
nayemshanto333 <nayemshantobd@gmail.com>
2024-10-14 23:30:01 +0600

    update faq section

--- GoverningDocs/src/compnents/Home/FAQ.jsx
+++ GoverningDocs/src/compnents/Home/FAQ.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Container } from "../../shareComponents/Container";
 import { SectionTitle } from "../../shareComponents/SectionTitle";
-import { Arrow } from "./Icon";
 import { faq } from "../../db/NavData";
 import FaqCard from "../../shareComponents/FaqCard";
 
@@ -9,15 +8,17 @@ function FAQ() {
     const [isActive, setIsActive] = useState();
     const handleChange =(index)=>{isActive == index ? setIsActive(-1) : setIsActive(index)}
   return (
-    <section className="my-9 md:my-20 lg:my-[120px] xl:my-[150px]">
-      <Container className="max-w-[982px]">
+    <section className="my-9 md:my-20 lg:my-[120px] xl:my-[150px] py-5">
+      <Container className="max-w-[982px] relative ">
         <SectionTitle gradient="Asked Questions" className="text-center">
           Frequently{" "}
         </SectionTitle>
         <div className="mt-10 space-y-5">
           {faq.map((faq , i) => <FaqCard key={i} {...faq} onClick={()=>handleChange(i)} isActive={isActive == i}/>)}
         </div>
+        <img className="absolute z-10  h-[590px] w-[1713px] -top-[110px] lg:-left-[21.5%] " src="img/faq-line.svg" alt="faq line" />
       </Container>
+      
     </section>
   );
 }
